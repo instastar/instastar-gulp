b12256d68299cdc55025b9c8cf0cc3d364457dc9
Darkhan <dakepromo@gmail.com>
2021-01-05 00:13:03 +0600

    с богом ребята

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
-function hello(hello) {
-   console.log(hello)
+function addUser(id, cont) {
+   $.instagramFeed({
+      'username': id, //Имя пользователя
+      'container': "#" + cont, //Контейнер для изображений
+      'display_profile': true, //Отображение профиля
+      'display_biography': false, //Отображение биографии
+      'display_gallery': true, //Отображение галереи
+      'styling': false, //Стандартные стили библиотеки
+      'items': 3, //Количество изображений в галереи
+      'items_per_row': 4, //Количество изображений в ряд
+      'margin': 0.5 //Отступ между изображениями
+  });
 }
-hello("hello world!!!")
+
+(function ($) {
+   $(window).on('load', function () {
+      addUser('ereke_007', 'ereke')
+   });
+})(jQuery);
