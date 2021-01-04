b55c5e9fe7c5a041f11b67c88a3388ffe2abb846
AssasinDEAD <64725895+AssasinDEAD@users.noreply.github.com>
2021-01-05 04:21:07 +0600

    end main block

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,19 @@ function addUser(id, cont) {
       'display_profile': true, //Отображение профиля
       'display_biography': false, //Отображение биографии
       'display_gallery': true, //Отображение галереи
-      'styling': false, //Стандартные стили библиотеки
+      'styling': true, //Стандартные стили библиотеки
       'items': 3, //Количество изображений в галереи
       'items_per_row': 4, //Количество изображений в ряд
       'margin': 0.5 //Отступ между изображениями
-  });
+   });
 }
 
 (function ($) {
    $(window).on('load', function () {
-      addUser('ereke_007', 'ereke')
+      addUser('_tolendiev_', 'galym')
+      addUser('nursila_003', 'nurs')
+      addUser('zhasulanankenov', 'zhas')
+      addUser('iluxa011', 'ilukha')
+      addUser('sonya___770', 'indira')
    });
 })(jQuery);
